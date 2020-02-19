c1244e254984004b9c9e6d8512062a7fb1a44201
Gavin Lafferty <59664725+Gplafferty0219@users.noreply.github.com>
2020-02-19 17:41:46 -0500

    Get latest close

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# robo-advisor
+# robo-advisor Project
+
+My soltuion to the Robo Advisor project:
+
+# installation
+clone or download this repository: onto your computer
+
+Navigate to it using a command line:
+ cd robo-advisor
+
+create an evironment using a command such as:
+ conda create -n stocks-env python=3.7#
+and then run the environment with:
+ conda activate stocks-env
+
+ 
--- app/robo.py
+++ app/robo.py
@@ -3,10 +3,14 @@
 import requests
 import json
 
+# Convert float or integer to usd formatting string
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 #
 # Info Inputs
 #
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo"
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
 
@@ -14,7 +18,7 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-
+latest_close = parsed_response["Time Series (Daily)"]["2020-02-19"]["4. close"]
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
@@ -22,8 +26,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: ", last_refreshed)
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
