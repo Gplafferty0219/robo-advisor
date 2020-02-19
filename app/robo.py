11143bf623197e236e444dcb863bbb3611a9cb71
Gavin Lafferty <59664725+Gplafferty0219@users.noreply.github.com>
2020-02-19 16:47:58 -0500

    Get latest refreshed data

--- app/robo.py
+++ app/robo.py
@@ -12,7 +12,8 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
-breakpoint()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
 
 
 print("-------------------------")
@@ -21,7 +22,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print("LATEST DAY: ", last_refreshed)
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
