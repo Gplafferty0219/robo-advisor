0493e1a3074f35c25411eb9dc4f0cd9a7e64b044
Gavin Lafferty <59664725+Gplafferty0219@users.noreply.github.com>
2020-02-23 19:15:05 -0500

    get latest high

--- app/robo.py
+++ app/robo.py
@@ -1,7 +1,11 @@
 # app/robo_advisor.py
+import csv
+import json
+import os
 
 import requests
-import json
+
+
 
 # Convert float or integer to usd formatting string
 def to_usd(my_price):
@@ -28,13 +32,25 @@ latest_close = tsd[latest_day]["4. close"]
 
 #get high price from each day
 high_prices = []
+low_prices = []
 
 for date in dates:
     high_price = tsd[date]["2. high"]
     high_prices.append(float(high_price))
+    low_price = tsd[date]["3. low"]
+    low_prices.append(float(low_price))
 
 #max of all prices
 recent_high = max(high_prices)
+recent_low = min(low_prices)
+
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data" , "prices.csv")
+
+with open(csv_file_path,something something):
+    blah
+    blah
+    blah
+    blah
 
 
 print("-------------------------")
@@ -46,10 +62,14 @@ print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
+print(f"WRITING DATA TO CSV {csv_file_path}...")
+print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+
