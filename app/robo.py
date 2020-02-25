264e7c148800ae977de9114a5e62af02899d2ec1
Gavin Lafferty <59664725+Gplafferty0219@users.noreply.github.com>
2020-02-24 20:33:49 -0500

    added a looping function to enter many stocks

--- app/robo.py
+++ app/robo.py
@@ -1,92 +1,117 @@
 # app/robo_advisor.py
+
+#IMPORT STUFFS
 import csv
 import json
 import os
 from dotenv import load_dotenv
 import requests
+import datetime
 
 load_dotenv()
 
-
-
-# Convert float or integer to usd formatting string
+#CONVERT TO $USD
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
 
-#
-# Info Inputs
-#
-api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+#READS THE API KEY OFF THE .ENV FILE
+api_key = os.getenv("ALPHAVANTAGE_API_KEY", default= "oops")
 
-symbol = "MSFT"
+#LOOPS THE FUNCTION SO YOU CAN ENTER MANY STOCKS
+while True:
+    #USER INPUT
+    symbol = input("Please enter a ticker or type 'DONE' when you are finished: ")
+    if symbol == "DONE":
+        exit()
+    print(len(symbol))
+    while True:
+        if str.isnumeric(symbol) or len(symbol) > 5:
+            print("Sorry")
+            symbol = input("Please enter a ticker or type 'DONE' when you are finished: ")
+        elif symbol == "DONE":
+            exit()
+        else:
+            break
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+    response = requests.get(request_url)
+    if "Error Message" in response.text:
+        print(f"Ticker '{symbol}' could not be found. Please try again.")
+        exit()
+    else:
+        parsed_response = json.loads(response.text)
 
-response = requests.get(request_url)
+    #LATEST DAY
+    last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-parsed_response = json.loads(response.text)
+    tsd = parsed_response["Time Series (Daily)"]
+    dates = list(tsd.keys())
 
-last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+    #LATEST CLOSE
+    latest_day = dates[0]
+    latest_close = tsd[latest_day]["4. close"]
 
-tsd = parsed_response["Time Series (Daily)"]
-
-dates = list(tsd.keys())
-
-latest_day = dates[0]
-latest_close = tsd[latest_day]["4. close"]
-
-#get high price from each day
-high_prices = []
-low_prices = []
-
-for date in dates:
-    high_price = tsd[date]["2. high"]
-    high_prices.append(float(high_price))
-    low_price = tsd[date]["3. low"]
-    low_prices.append(float(low_price))
-
-#max of all prices
-recent_high = max(high_prices)
-recent_low = min(low_prices)
-
-
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data" , "prices.csv")
-
-csv_headers = ("timestamp", "open", "high", "low", "close", "volume")
-
-with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader()
+    #PRICING MODEL
+    high_prices = []
+    low_prices = []
     for date in dates:
-        daily_prices = tsd[date]
-        writer.writerow({
-            "timestamp": date,
-            "open": daily_prices["1. open"],
-            "high": daily_prices["2. high"],
-            "low": daily_prices["3. low"],
-            "close": daily_prices["4. close"],
-            "volume": daily_prices["5. volume"]
-        })
+        high_price = tsd[date]["2. high"]
+        high_prices.append(float(high_price))
+        low_price = tsd[date]["3. low"]
+        low_prices.append(float(low_price))
+    #RECENT HIGH
+    recent_high = max(high_prices)
+    #RECENT LOW
+    recent_low = min(low_prices)
+
+    #IMPORT DATA TO PRICES FILE
+    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data" , "prices.csv")
+    csv_headers = ("timestamp", "open", "high", "low", "close", "volume")
+    with open(csv_file_path, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+        writer.writeheader()
+        for date in dates:
+            daily_prices = tsd[date]
+            writer.writerow({
+                "timestamp": date,
+                "open": daily_prices["1. open"],
+                "high": daily_prices["2. high"],
+                "low": daily_prices["3. low"],
+                "close": daily_prices["4. close"],
+                "volume": daily_prices["5. volume"]
+            })
+
+    #REQUEST TIME
+    today = datetime.datetime.today()
 
 
-print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
-print("-------------------------")
-print(f"LATEST DAY: {last_refreshed}")
-print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-print(f"RECENT LOW: {to_usd(float(recent_low))}")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print(f"WRITING DATA TO CSV {csv_file_path}...")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
-
-
-
+    #-----------------------------------------#
+    #RECEIPT
+    print("-------------------------")
+    print(f"SELECTED SYMBOL: {symbol}")
+    print("-------------------------")
+    print("REQUESTING STOCK MARKET DATA...")
+    print("REQUEST MADE AT: ", today.strftime("%m/%d/%Y %I:%M %p"))
+    print("-------------------------")
+    print(f"LATEST DAY: {last_refreshed}")
+    print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+    print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+    print(f"RECENT LOW: {to_usd(float(recent_low))}")
+    print("-------------------------")
+    if float(latest_close) >= (0.80*(float(recent_high))) and float(latest_close) <= (1.20*(float(recent_low))):
+        print("RECOMMENDATION: HOLD")
+        print("RECOMMENDATION: Your stock is not very volatile. Hold for and hope for some steady returns.")
+    elif float(latest_close) <= (1.20*(float(recent_low))):
+        print("RECOMMENDATION: BUY!") 
+        print("RECOMMENDATION REASON: The stock is at a relatively low price. Buy now before it rises!")
+    elif float(latest_close) >= (0.80*(float(recent_high))):
+        print("RECOMMENDATION: SELL!") 
+        print("RECOMMENDATION REASON: The stock is at a relatively high price. Consider cashing out on it now!")
+    else:
+        print("RECOMMENDATION: HOLD")
+        print("RECOMMENDATION: Your stock is not moving too much. Hold for now and hope for a good performance soon.")
+    print("-------------------------")
+    print(f"WRITING DATA TO CSV {csv_file_path}...")
+    print("-------------------------")
+    print("HAPPY INVESTING!")
+    print("-------------------------")
